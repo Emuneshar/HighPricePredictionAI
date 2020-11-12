174bd3ff23631757c36c042b5553fed8f988ec74
hs115488 <haresh.singh.97@gmail.com>
2020-11-12 13:52:31 -0500

    Added forecasting models

--- Code/PricePredictionAI.py
+++ Code/PricePredictionAI.py
@@ -2,6 +2,7 @@ from WebScraper import Ticker
 from fbprophet import Prophet
 import pandas as pd
 import numpy as np
+from stocker import Stocker
 
 
 class PredictionAI:
@@ -17,7 +18,7 @@ class PredictionAI:
             data.append(current_price)
 
 
-    def forecast(self, data):
+    def prophet(self, data):
         data = pd.Dataframe(data)
         data.columns("Price")
 
@@ -31,6 +32,25 @@ class PredictionAI:
 
         def components():
             fig2 = m.plot_components(forecast)
+
+    class Forecast:
+        def __init__(self, ticker_symbol):
+            self.stock = Stocker(ticker_symbol)
+
+        def price_history(self):
+            self.stock.plot_stock()
+
+        def model(self):
+            model, model_data = self.stock.create_prophet_model(days=90)
+
+        def prediction(self):
+            self.stock.evaluate_prediction()
+
+        def prediction_with_shares(self, shares):
+            self.stock.evaluate_prediction(nshares=shares)
+
+        def predict_future(self, days):
+            self.stock.predict_future(days=days)
 
     def neural_network(self):
         #TODO: Create a tensorflow model(CNN)
