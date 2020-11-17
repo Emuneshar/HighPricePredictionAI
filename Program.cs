5c018637b17b37f054cac226d1cf38df0ae6140e
hs115488 <haresh.singh.97@gmail.com>
2020-11-17 15:05:43 -0500

    Added .NET Core Scraper

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HighPricePredictionAI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
